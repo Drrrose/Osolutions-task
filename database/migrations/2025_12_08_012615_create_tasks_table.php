6728dcbf0e4a3807b1f57930a1f1c11617bab7a7
Drrrose <https://Drrrose@github.com>
2025-12-08 03:31:43 +0200

    create app + migration

---
+++ database/migrations/2025_12_08_012615_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('priority')->default('medium'); // didn't use enum for simplicity sometimes if enum is changed later it can be problematic
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->date('due_date');
+            $table->boolean('completed')->default(false);
+            $table->string('image_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
